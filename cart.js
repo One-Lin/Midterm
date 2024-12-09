b95e4f4bbc9b42aec5681218f60084f790653f99
One-lin <asd147267@gmail.com>
2024-12-09 23:44:37 +0800

    新增購物車

--- cart.js
+++ cart.js
@@ -1,20 +1,10 @@
-let cart = [];
+let iconCart = document.querySelector('.icon-cart');
+let closeCart = document.querySelector('.close');
+let body = document.querySelector('body');
 
-        function addToCart(productName, productPrice) {
-            cart.push({ name: productName, price: productPrice });
-            updateCart();
-        }
-
-        function updateCart() {
-            const cartItemsElement = document.getElementById('cart-items');
-            const totalElement = document.getElementById('total');
-            cartItemsElement.innerHTML = '';
-
-            let total = 0;
-            cart.forEach((item, index) => {
-                cartItemsElement.innerHTML += `<div class="cart-item">${item.name} - NT$${item.price}</div>`;
-                total += item.price;
-            });
-
-            totalElement.textContent = total;
-        }
+iconCart.addEventListener('click',()=>{
+    body.classList.toggle('showCart')
+})
+closeCart.addEventListener('click',()=>{
+    body.classList.toggle('showCart')
+})
