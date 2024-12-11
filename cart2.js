1fcfbcb9b31123ae408e8fc3e6b97fa6524de3a5
One-Lin <asd147267@gmail.com>
2024-12-11 14:40:41 +0800

    新增關於本店

--- cart2.js
+++ cart2.js
@@ -8,24 +8,25 @@ function showContact() {
     document.body.classList.toggle('showContact');
 }
 
+//顯示全部商品
 function showAll() {
     document.querySelectorAll('.item').forEach(div => {
         div.classList.remove('hideProduct');
-      });
+    });
     //   alert(123);
 }
 
-function show(targetClass){
-    
+// 顯示特定分類商品
+function show(targetClass) {
     document.querySelectorAll('.item').forEach(div => {
         div.classList.add('hideProduct');
-      });
-      const targetDiv = document.querySelectorAll(`.${targetClass}`);
-      if (targetDiv) {
+    });
+    const targetDiv = document.querySelectorAll(`.${targetClass}`);
+    if (targetDiv) {
         targetDiv.forEach(div => {
             div.classList.toggle('hideProduct');
             div.classList.add('showProduct');
-          });;
-      }
+        });;
+    }
     // alert(456);   
 }
