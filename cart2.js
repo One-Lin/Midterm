ae810478f0c946c8b93452ae34dc78d915a6a4f8
One-Lin <asd147267@gmail.com>
2024-12-11 11:44:30 +0800

    完成分類標籤

--- cart2.js
+++ cart2.js
@@ -7,3 +7,25 @@ function showCart() {
 function showContact() {
     document.body.classList.toggle('showContact');
 }
+
+function showAll() {
+    document.querySelectorAll('.item').forEach(div => {
+        div.classList.remove('hideProduct');
+      });
+    //   alert(123);
+}
+
+function show(targetClass){
+    
+    document.querySelectorAll('.item').forEach(div => {
+        div.classList.add('hideProduct');
+      });
+      const targetDiv = document.querySelectorAll(`.${targetClass}`);
+      if (targetDiv) {
+        targetDiv.forEach(div => {
+            div.classList.toggle('hideProduct');
+            div.classList.add('showProduct');
+          });;
+      }
+    // alert(456);   
+}
