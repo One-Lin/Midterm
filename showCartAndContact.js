3b96bff24990b9c5ac347a7919a601394d133a93
One-lin <asd147267@gmail.com>
2024-12-13 15:57:54 +0800

    finalV4

--- showCartAndContact.js
+++ showCartAndContact.js
@@ -8,3 +8,19 @@ function showContact() {
     document.body.classList.toggle('showContact');
 }
 
+//送出
+function submitContact() {
+    let name = document.getElementById('Username').value;
+    let phone = document.getElementById('Phone').value;
+    let email = document.getElementById('Email').value;
+    let opinion= document.getElementById('opinion').value;
+
+    if (name && phone && email && opinion ) {
+        alert('我們已收到寶貴意見，謝謝您');
+        showContact();
+    }
+    else {
+        alert('請確認是否完整填寫，謝謝您');
+    }
+    // alert(name);
+}
